cd0c97667e271674194336ab04bc91ddf1812b84
PedroHLC <root@pedrohlc.com>
2021-12-13 11:33:01 -0300

    Move to Nix

--- 71-zfs-hoook.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -o errexit
-cd /mnt
-
-# HOOKS
-sed -i'' "s/filesystems keyboard/keyboard zfs filesystems/g" ./etc/mkinitcpio.conf
-
-echo 'Finished'
---
+++ configuration.nix
@@ -0,0 +1,166 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+let
+  slack = pkgs.slack.overrideAttrs (old: {
+    installPhase = old.installPhase + ''
+      rm $out/bin/slack
+
+      makeWrapper $out/lib/slack/slack $out/bin/slack \
+        --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+        --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
+        --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+    '';
+  });
+
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.requestEncryptionCredentials = false;
+
+  networking.hostId = "0f8623ae";
+  networking.hostName = "laptop"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour
+  networking.useDHCP = false;
+  networking.interfaces.enp2s0.useDHCP = true;
+  networking.interfaces.wlo1.useDHCP = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "br-abnt2";
+  };
+
+  # Enable the SwayWM.
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      wl-clipboard
+      mako
+      alacritty
+    ];
+    extraSessionCommands = ''
+      export BEMENU_BACKEND='wayland'
+      export CLUTTER_BACKEND='wayland'
+      export ECORE_EVAS_ENGINE='wayland_egl'
+      export ELM_ENGINE='wayland_egl'
+      export GDK_BACKEND='wayland'
+      export MOZ_ENABLE_WAYLAND=1
+      export QT_AUTO_SCREEN_SCALE_FACTOR=0
+      export QT_PLATFORM_PLUGIN='kde'
+      export QT_PLATFORMTHEME='kde'
+      export QT_QPA_PLATFORM='wayland-egl'
+      export QT_QPA_PLATFORMTHEME='kde'
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      export SAL_USE_VCLPLUGIN='gtk3'
+      export SDL_VIDEODRIVER='wayland'
+      export _JAVA_AWT_WM_NONREPARENTING=1
+    '';
+  };
+  xdg.portal.wlr.enable = true;
+  services.xserver.layout = "br";
+
+  # Enable sound.
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pedrohlc = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    firefox
+    tdesktop
+    brightnessctl
+    slack
+    acpid
+    breeze-qt5
+    breeze-gtk
+    breeze-icons
+    killall
+    vulkan-tools
+    git
+    tmux
+    mpv
+
+    fira
+    fira-code
+    fira-code-symbols
+    fira-mono
+    cantarell-fonts
+    freefont_ttf
+    noto-fonts
+    ubuntu_font_family
+    google-fonts
+    liberation_ttf
+
+    yambar-wayland
+    sublime4
+    yarn
+    elmPackages.elm-format
+    vimix-icon-theme
+    font-awesome_5
+    lxqt.pavucontrol-qt
+    lxqt.pcmanfm-qt
+  ];
+  programs.fish.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
+
