d404fa51ab00816e9b6c8f214d50883de8b187fc
PedroHLC <root@pedrohlc.com>
2022-04-24 15:23:56 -0300

    [wireguard] Add POCO X3

--- desktop/wireguard.nix
+++ desktop/wireguard.nix
@@ -14,12 +14,23 @@
           listenPort = 51820;
           privateKeyFile = "/home/pedrohlc/Projects/com.pedrohlc/wireguard-keys/private";
           peers = [
+            # Laptop
             {
-              # Laptop
               publicKey = "sS6SMVRPPvTGdjVBUScWkYqT8jjT8PIWy0kzMklwITM=";
               allowedIPs = [
                 "10.100.0.2/32"
                 "fda4:4413:3bb1::2/128"
+                # Multicast
+                "224.0.0.251/32"
+                "ff02::fb/128"
+              ];
+            }
+            # POCO X3
+            {
+              publicKey = "j6bZsZZoWfN4SaJuCxP2ndqWGc75A2JH3gxNwSbIDEM=";
+              allowedIPs = [
+                "10.100.0.3/32"
+                "fda4:4413:3bb1::3/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
