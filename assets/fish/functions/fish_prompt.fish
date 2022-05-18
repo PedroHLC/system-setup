6a6fd77d7c652f9a31896a376628ae775e0e9fe3
PedroHLC ☭ <root@pedrohlc.com>
2022-05-18 09:28:00 -0300

    [fish] Add theme-lambda file

---
+++ assets/fish/functions/fish_prompt.fish
@@ -0,0 +1,51 @@
+# File originally from: hasanozgan/theme-lambda
+
+function fish_prompt
+  # Cache exit status
+  set -l last_status $status
+
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+  if not set -q __fish_prompt_char
+    switch (id -u)
+      case 0
+	set -g __fish_prompt_char '#'
+      case '*'
+	set -g __fish_prompt_char 'λ'
+    end
+  end
+
+  # Setup colors
+  #use extended color pallete if available
+  set -l normal (set_color normal)
+  set -l white (set_color FFFFFF)
+  set -l turquoise (set_color 5fdfff)
+  set -l orange (set_color df5f00)
+  set -l hotpink (set_color df005f)
+  set -l blue (set_color blue)
+  set -l limegreen (set_color 87ff00)
+  set -l purple (set_color af5fff)
+ 
+  # Configure __fish_git_prompt
+  set -g __fish_git_prompt_char_stateseparator ' '
+  set -g __fish_git_prompt_color 5fdfff
+  set -g __fish_git_prompt_color_flags df5f00
+  set -g __fish_git_prompt_color_prefix white
+  set -g __fish_git_prompt_color_suffix white
+  set -g __fish_git_prompt_showdirtystate true
+  set -g __fish_git_prompt_showuntrackedfiles true
+  set -g __fish_git_prompt_showstashstate true
+  set -g __fish_git_prompt_show_informative_status true 
+ 
+  # Line 1
+  echo -n $white'╭─'$hotpink$USER$white' at '$orange$__fish_prompt_hostname$white' in '$limegreen(pwd)$turquoise
+  __fish_git_prompt " (%s)"
+  echo
+
+  # Line 2
+  echo -n $white'╰─'$__fish_prompt_char $normal
+end
+
+
