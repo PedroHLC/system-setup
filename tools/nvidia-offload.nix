d09cc6bd27b2d2296efa17ae0d92c53f63335438
PedroHLC ☭ <root@pedrohlc.com>
2022-06-27 08:12:26 -0300

    [nvidia] Adopt changes from nixos-hardware #427

--- tools/nvidia-offload.nix
+++ tools/nvidia-offload.nix
@@ -6,5 +6,5 @@ writeShellScriptBin "nvidia-offload" ''
   export __VK_LAYER_NV_optimus="NVIDIA_only"
   export VK_ICD_FILENAMES="/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json"
   export LIBVA_DRIVER_NAME="nvidia"
-  exec -a "$0" "$@"
+  exec "$@"
 ''
