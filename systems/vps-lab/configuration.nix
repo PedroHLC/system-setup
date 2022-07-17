839f572eb2cb16e6188fb50258f7c94af29cf2e3
PedroHLC ☭ <root@pedrohlc.com>
2022-07-17 13:53:04 -0300

    [vps-lab] finishes config

--- systems/vps-lab/configuration.nix
+++ systems/vps-lab/configuration.nix
@@ -13,4 +13,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
+  home-manager.users.pedrohlc.home.stateVersion = "22.05";
 }
