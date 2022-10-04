ad1b74161d43342eb60934629cc270900312d5cb
PedroHLC ☭ <root@pedrohlc.com>
2022-10-04 10:46:33 -0300

    [vps-lab] Complete NAT setup

--- systems/vps-lab/servers/wireguard.nix
+++ systems/vps-lab/servers/wireguard.nix
@@ -1,16 +1,23 @@
 { pkgs, ... }:
+let
+  eth0 = "enp0s3";
+
+  wgPrefixV4 = "10.100.0";
+  wgPrefixV6 = "fda4:4413:3bb1:";
+in
 {
   networking = {
     nat = {
       enable = true;
-      externalInterface = "enp0s3";
+      enableIPv6 = true;
+      externalInterface = eth0;
       internalInterfaces = [ "wg0" ];
     };
 
     wireguard = {
       interfaces = {
         wg0 = {
-          ips = [ "10.100.0.1/24" "fda4:4413:3bb1::1/64" ];
+          ips = [ "${wgPrefixV4}.1/24" "${wgPrefixV6}:1/64" ];
           listenPort = 51820;
           privateKeyFile = "/home/pedrohlc/Projects/com.pedrohlc/wireguard-keys/private";
           peers = [
@@ -18,8 +25,8 @@
             {
               publicKey = "cU6dpSqyloVRf6Jjb84TygJO94NOCy+LnMYv6/QAbBs=";
               allowedIPs = [
-                "10.100.0.2/32"
-                "fda4:4413:3bb1::2/128"
+                "${wgPrefixV4}.2/32"
+                "${wgPrefixV6}:2/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
@@ -29,8 +36,8 @@
             {
               publicKey = "sS6SMVRPPvTGdjVBUScWkYqT8jjT8PIWy0kzMklwITM=";
               allowedIPs = [
-                "10.100.0.3/32"
-                "fda4:4413:3bb1::3/128"
+                "${wgPrefixV4}.3/32"
+                "${wgPrefixV6}:3/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
@@ -40,8 +47,8 @@
             {
               publicKey = "j6bZsZZoWfN4SaJuCxP2ndqWGc75A2JH3gxNwSbIDEM=";
               allowedIPs = [
-                "10.100.0.4/32"
-                "fda4:4413:3bb1::4/128"
+                "${wgPrefixV4}.4/32"
+                "${wgPrefixV6}:4/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
@@ -51,8 +58,8 @@
             {
               publicKey = "tIQzW4+qfv2V8aCLwxJnWRnF+pjV3yRxTRuCPnA2CEA=";
               allowedIPs = [
-                "10.100.0.202/32"
-                "fda4:4413:3bb1::202/128"
+                "${wgPrefixV4}.202/32"
+                "${wgPrefixV6}:202/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
@@ -62,8 +69,8 @@
             {
               publicKey = "kBVBMFXlj9m6TTy0nBeKBOyaqo5ulBUaPddFTTA2CFc=";
               allowedIPs = [
-                "10.100.0.203/32"
-                "fda4:4413:3bb1::203/128"
+                "${wgPrefixV4}.203/32"
+                "${wgPrefixV6}:203/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
@@ -73,8 +80,8 @@
             {
               publicKey = "sJgsAYeBx0aE8es0Yfps0f7fBRpUvcxgyB19EM7wi1M=";
               allowedIPs = [
-                "10.100.0.204/32"
-                "fda4:4413:3bb1::204/128"
+                "${wgPrefixV4}.204/32"
+                "${wgPrefixV6}:204/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
@@ -84,8 +91,8 @@
             {
               publicKey = "VIDFaQvPJKkE6Ppc/PSjXt1TuzQMFhF5wL6qytKi5zk=";
               allowedIPs = [
-                "10.100.0.205/32"
-                "fda4:4413:3bb1::205/128"
+                "${wgPrefixV4}.205/32"
+                "${wgPrefixV6}:205/128"
                 # Multicast
                 "224.0.0.251/32"
                 "ff02::fb/128"
@@ -94,16 +101,25 @@
 
           ];
           postSetup = ''
-            ip link set wg0 multicast on
+                        ip link set wg0 multicast on
+            	    ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+            	    ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${wgPrefixV4}.0/24 -o ${eth0} -j MASQUERADE
+            	    ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
+            	    ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s ${wgPrefixV6}:0/64 -o ${eth0} -j MASQUERADE
+          '';
+          postShutdown = ''
+            ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${wgPrefixV4}.0/24 -o ${eth0} -j MASQUERADE
+            ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
+            ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s ${wgPrefixV6}:0/24 -o ${eth0} -j MASQUERADE
           '';
         };
       };
     };
   };
 
-  # ip forwarding
+  # ip forwarding (missing NAT sysctl)
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
   };
 }
