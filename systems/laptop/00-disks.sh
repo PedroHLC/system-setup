11f1a808f2b977687ba61461f25d4006aa3d5987
PedroHLC ☭ <root@pedrohlc.com>
2022-10-30 10:54:27 -0300

    Revert "[laptop][wip] refactor fs to adopt impermanence through ZFS"
    - This reverts commit 1e9064f6840c9de4fcb8a96c2fef66e3758ab0cc.
    - This reverts commit 941fbb024f09766813abd0565bb1722ef33ae0da.
    - This reverts commit f074cb50c232400e2d88665327f68c4ee3f9c3ad.
    - This reverts commit 4b7446a9a5b2dd589b655e3f443d263fa2888027.
    - This reverts commit de416ab163066d8d3d08a1ef210dc3dd5d706272.

--- systems/laptop/00-disks.sh
+++ systems/laptop/00-disks.sh
@@ -1,104 +1,37 @@
 #!/usr/bin/env sh
 set -o errexit
 
-# Swaps.
-#mkswap /dev/nvme0n1p3 # (TODO)
-#swapon /dev/nvme0n1p3 # (TODO)
-
 # Create EFI.
 mkfs.vfat -F32 /dev/nvme0n1p1
 
-# Create ZFS pool.
+# Create pool.
 zpool create -f zroot /dev/nvme0n1p2
 zpool set autotrim=on zroot
 zfs set compression=lz4 zroot
 zfs set mountpoint=none zroot
 
-# Top-level hierarchy.
+# System volumes.
 zfs create -o mountpoint=none zroot/data
 zfs create -o mountpoint=none zroot/ROOT
-zfs create -o mountpoint=none zroot/apps
-zfs create -o mountpoint=none zroot/guests
-zfs create -o mountpoint=none -o recordsize=1M zroot/games
+zfs create -o mountpoint=/ zroot/ROOT/default
+zfs create -o mountpoint=/home zroot/data/home
+#zfs create -o mountpoint=/etc/ssh zroot/data/ssh # (TODO)
+#zfs create -o mountpoint=/etc/NetworkManager/system-connections zroot/data/connections # (TODO)
+#zfs create -o mountpoint=/nix zroot/nix # (TODO)
 
-# System's filesystem
-# zfs create -o mountpoint=/ zroot/ROOT/default # (LEGACY)
-zfs create -o mountpoint=/ zroot/ROOT/empty
-zfs create -o mountpoint=/nix zroot/ROOT/nix
-zfs create -o mountpoint=/var/cache zroot/ROOT/var-cache
-zfs create -o mountpoint=/var/log zroot/ROOT/var-log
+# Different recordsize
+zfs create -o mountpoint=/home/pedrohlc/.cache/btdownloads -o recordsize=16K zroot/data/btdownloads 
+#zfs create -o mountpoint=/home/pedrohlc/.local/share/Steam/steamapps/common zroot/games/steam # (TODO)
+#zfs create -o mountpoint=/home/pedrohlc/Games zroot/games/home # (TODO)
 
-# System's data
-zfs create -o mountpoint=/etc/NetworkManager/system-connections zroot/data/connections
-zfs create -o mountpoint=/etc/nixos zroot/data/setup
-zfs create -o mountpoint=/etc/ssh zroot/data/sshd
-zfs create -o mountpoint=/var/lib/bluetooth zroot/data/bluetooth
-zfs create -o mountpoint=/var/lib/containers zroot/data/containers
-zfs create -o mountpoint=/var/lib/flatpak zroot/data/flatpak
-zfs create -o mountpoint=/var/lib/postgresql \
-	-o recordsize=16K -o primarycache=metadata \
-	zroot/data/postgres
-zfs create -o mountpoint=/var/lib/systemd zroot/data/systemd
-zfs create -o mountpoint=/var/lib/upower zroot/data/upower
-
-# Root's home
-zfs create -o mountpoint=/root/.cache zroot/data/root-cache
-zfs create -o mountpoint=/root/.gnupg zroot/data/root-gnupg
-zfs create -o mountpoint=/root/.persistent zroot/data/root-files
-zfs create -o mountpoint=/root/.ssh zroot/data/root-ssh
-
-# Pedro's home
-# zfs create -o mountpoint=/home zroot/data/home # (LEGACY)
-zfs create -o mountpoint=/home/pedrohlc/.cache zroot/data/my-cache
-zfs create -o mountpoint=/home/pedrohlc/.gnupg zroot/data/my-gnupg
-zfs create -o mountpoint=/home/pedrohlc/.local/share/containers zroot/data/my-containers
-zfs create -o mountpoint=/home/pedrohlc/.local/share/Trash zroot/data/my-trash
-zfs create -o mountpoint=/home/pedrohlc/.persistent zroot/data/my-files
-zfs create -o mountpoint=/home/pedrohlc/.ssh zroot/data/my-ssh
-zfs create -o mountpoint=/home/pedrohlc/Downloads -o recordsize=16K zroot/data/btdownloads
-zfs create -o mountpoint=/home/pedrohlc/Projects zroot/data/my-projects
-
-# Pedro's apps
-zfs create -o mountpoint=/home/pedrohlc/.aws zroot/apps/aws
-zfs create -o mountpoint=/home/pedrohlc/.config/btop zroot/apps/btop
-zfs create -o mountpoint=/home/pedrohlc/.config/discord zroot/apps/discord
-zfs create -o mountpoint=/home/pedrohlc/.config/Element zroot/apps/element
-zfs create -o mountpoint=/home/pedrohlc/.config/Keybase zroot/apps/keybase-gui
-zfs create -o mountpoint=/home/pedrohlc/.config/keybase zroot/apps/keybase-core
-zfs create -o mountpoint=/home/pedrohlc/.config/nvim zroot/apps/nvim
-zfs create -o mountpoint=/home/pedrohlc/.config/obs-studio zroot/apps/obs-studio
-zfs create -o mountpoint=/home/pedrohlc/.config/qBittorrent zroot/apps/qbittorrent
-zfs create -o mountpoint=/home/pedrohlc/.config/spotify zroot/apps/spotify
-zfs create -o mountpoint=/home/pedrohlc/.config/sublime-text zroot/apps/subl
-zfs create -o mountpoint=/home/pedrohlc/.config/TabNine zroot/apps/tabnine
-zfs create -o mountpoint=/home/pedrohlc/.kube zroot/apps/kube
-zfs create -o mountpoint=/home/pedrohlc/.local/share/DBeaverData zroot/apps/dbeaver
-zfs create -o mountpoint=/home/pedrohlc/.local/share/fish zroot/apps/fish
-zfs create -o mountpoint=/home/pedrohlc/.local/share/keybase zroot/apps/keybase-data
-zfs create -o mountpoint=/home/pedrohlc/.local/share/Steam zroot/apps/steam
-zfs create -o mountpoint=/home/pedrohlc/.local/share/TelegramDesktop zroot/apps/tdesktop
-zfs create -o mountpoint=/home/pedrohlc/.local/share/Terraria zroot/apps/terraria-saves
-zfs create -o mountpoint=/home/pedrohlc/.zoom zroot/apps/zoom
-
-# Pedro's games
-zfs create -o mountpoint=/home/pedrohlc/.local/share/Steam/steamapps/common zroot/games/steam
-zfs create -o mountpoint=/home/pedrohlc/Games zroot/games/home
-
-# Pedro's encrypted directories
+# Encrypted volumes.
 zfs create -o encryption=on -o keyformat=passphrase \
-	-o mountpoint=/home/pedrohlc/.encrypted zroot/data/encrypted
+	-o mountpoint=/media/encrypted zroot/data/encrypted
 zfs create -o encryption=on -o keyformat=passphrase \
 	-o mountpoint=/home/pedrohlc/.mozilla zroot/data/mozilla
 
-# Guests' homes
-zfs create -o mountpoint=/home/melinapn zroot/guests/melinapn
-
-sudo chown -R 1001:100 /home/pedrohlc
-sudo chown -R 1002:100 /home/melinapn
-sudo chmod -R -077 /home/{pedrohlc,melinapn} /root
-
-# Later on
-# sudo chown -R postgres:postgres /var/lib/postgresql
-# sudo chmod 750 /var/lib/postgresql
+# Swaps.
+#mkswap /dev/nvme0n1p3 # (TODO)
+#swapon /dev/nvme0n1p3 # (TODO)
 
 echo 'Finished. After installing NixOS, change every mountpoint to legacy.'
--- systems/laptop/hardware-configuration.nix
+++ systems/laptop/hardware-configuration.nix
@@ -1,12 +1,5 @@
 { config, lib, modulesPath, ... }:
 
-let
-  zfsFs = path:
-    {
-      device = "zroot/${path}";
-      fsType = "zfs";
-    };
-in
 {
   imports =
     [
@@ -18,73 +11,23 @@ in
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  # System's filesystem
-  fileSystems."/" = zfsFs "ROOT/empty";
-  fileSystems."/nix" = zfsFs "ROOT/nix";
-  fileSystems."/var/cache" = zfsFs "ROOT/var-cache";
-  fileSystems."/var/log" = zfsFs "ROOT/var-log";
+  fileSystems."/" =
+    {
+      device = "zroot/ROOT/default";
+      fsType = "zfs";
+    };
 
-  # System's data
-  fileSystems."/etc/NetworkManager/system-connections" = zfsFs "data/connections";
-  fileSystems."/etc/nixos" = zfsFs "data/setup";
-  fileSystems."/etc/ssh" = zfsFs "data/sshd";
-  fileSystems."/var/lib/bluetooth" = zfsFs "data/bluetooth";
-  fileSystems."/var/lib/containers" = zfsFs "data/containers";
-  fileSystems."/var/lib/flatpak" = zfsFs "data/flatpak";
-  fileSystems."/var/lib/postgresql" = zfsFs "data/postgres";
-  fileSystems."/var/lib/systemd" = zfsFs "data/systemd";
-  fileSystems."/var/lib/upower" = zfsFs "data/upower";
+  fileSystems."/home" =
+    {
+      device = "zroot/data/home";
+      fsType = "zfs";
+    };
 
-  # Root's home
-  fileSystems."/root/.cache" = zfsFs "data/root-cache";
-  fileSystems."/root/.gnupg" = zfsFs "data/root-gnupg";
-  fileSystems."/root/.persistent" = zfsFs "data/root-files";
-  fileSystems."/root/.ssh" = zfsFs "data/root-ssh";
-
-  # Pedro's home
-  fileSystems."/home/pedrohlc/.cache" = zfsFs "data/my-cache";
-  fileSystems."/home/pedrohlc/.gnupg" = zfsFs "data/my-gnupg";
-  fileSystems."/home/pedrohlc/.local/share/containers" = zfsFs "data/my-containers";
-  fileSystems."/home/pedrohlc/.local/share/Trash" = zfsFs "data/my-trash";
-  fileSystems."/home/pedrohlc/.persistent" = zfsFs "data/my-files";
-  fileSystems."/home/pedrohlc/.ssh" = zfsFs "data/my-ssh";
-  fileSystems."/home/pedrohlc/Downloads" = zfsFs "data/btdownloads";
-  fileSystems."/home/pedrohlc/Projects" = zfsFs "data/my-projects";
-
-  # Pedro's apps
-  fileSystems."/home/pedrohlc/.aws" = zfsFs "apps/aws";
-  fileSystems."/home/pedrohlc/.config/btop" = zfsFs "apps/btop";
-  fileSystems."/home/pedrohlc/.config/discord" = zfsFs "apps/discord";
-  fileSystems."/home/pedrohlc/.config/Element" = zfsFs "apps/element";
-  fileSystems."/home/pedrohlc/.config/Keybase" = zfsFs "apps/keybase-gui";
-  fileSystems."/home/pedrohlc/.config/keybase" = zfsFs "apps/keybase-core";
-  fileSystems."/home/pedrohlc/.config/nvim" = zfsFs "apps/nvim";
-  fileSystems."/home/pedrohlc/.config/obs-studio" = zfsFs "apps/obs-studio";
-  fileSystems."/home/pedrohlc/.config/qBittorrent" = zfsFs "apps/qbittorrent";
-  fileSystems."/home/pedrohlc/.config/spotify" = zfsFs "apps/spotify";
-  fileSystems."/home/pedrohlc/.config/sublime-text" = zfsFs "apps/subl";
-  fileSystems."/home/pedrohlc/.config/TabNine" = zfsFs "apps/tabnine";
-  fileSystems."/home/pedrohlc/.kube" = zfsFs "apps/kube";
-  fileSystems."/home/pedrohlc/.local/share/DBeaverData" = zfsFs "apps/dbeaver";
-  fileSystems."/home/pedrohlc/.local/share/fish" = zfsFs "apps/fish";
-  fileSystems."/home/pedrohlc/.local/share/keybase" = zfsFs "apps/keybase-data";
-  fileSystems."/home/pedrohlc/.local/share/Steam" = zfsFs "apps/steam";
-  fileSystems."/home/pedrohlc/.local/share/TelegramDesktop" = zfsFs "apps/tdesktop";
-  fileSystems."/home/pedrohlc/.local/share/Terraria" = zfsFs "apps/terraria-saves";
-  fileSystems."/home/pedrohlc/.zoom" = zfsFs "apps/zoom";
-
-  # Pedro's games
-  fileSystems."/home/pedrohlc/.local/share/Steam/steamapps/common" = zfsFs "games/steam";
-  fileSystems."/home/pedrohlc/Games" = zfsFs "games/home";
-
-  # Guests' homes
-  fileSystems."/home/melinapn" = zfsFs "guests/melinapn";
-
-  # Legacy
-  fileSystems."/mnt" = zfsFs "ROOT/default";
-  fileSystems."/mnt/home" = zfsFs "data/home";
-
-  # ...
+  fileSystems."/home/pedrohlc/.cache/btdownloads" =
+    {
+      device = "zroot/data/btdownloads";
+      fsType = "zfs";
+    };
 
   fileSystems."/boot" =
     {
