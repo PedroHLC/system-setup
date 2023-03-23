d2f6db680aa35b96b512cd575400fdd75faa6a5a
PedroHLC ☭ <root@pedrohlc.com>
2023-03-22 21:43:11 -0300

    [desktop] add the AUX1-only loopback

--- shared/pkgs/pw-focusrite-mono-input.nix
+++ shared/pkgs/pw-focusrite-mono-input.nix
@@ -7,7 +7,7 @@ json.generate "focusrite-mono-input.conf" {
     {
       name = "libpipewire-module-loopback";
       args = {
-        "node.description" = "Scarlett Solo (3rd Gen.) Pro Mono Input";
+        "node.description" = "Scarlett Solo (3rd Gen.) Pro Mic Input";
         "capture.props" = {
           "node.name" = "capture.P7AY7C60A16FA7-mic";
           "audio.position" = [ "FL" ];
@@ -17,6 +17,24 @@ json.generate "focusrite-mono-input.conf" {
         };
         "playback.props" = {
           "node.name" = "P7AY7C60A16FA7-mic";
+          "media.class" = "Audio/Source";
+          "audio.position" = [ "MONO" ];
+        };
+      };
+    }
+    {
+      name = "libpipewire-module-loopback";
+      args = {
+        "node.description" = "Scarlett Solo (3rd Gen.) Pro Instrument Input";
+        "capture.props" = {
+          "node.name" = "capture.P7AY7C60A16FA7-inst";
+          "audio.position" = [ "FR" ];
+          "node.target" = "alsa_input.usb-Focusrite_Scarlett_Solo_USB_P7AY7C60A16FA7-00.pro-input-0";
+          "stream.dont-remix" = true;
+          "node.passive" = true;
+        };
+        "playback.props" = {
+          "node.name" = "P7AY7C60A16FA7-inst";
           "media.class" = "Audio/Source";
           "audio.position" = [ "MONO" ];
         };
