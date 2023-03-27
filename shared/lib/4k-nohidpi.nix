b1d677d9a92ba8dcfda6376916a3b1d3770951db
PedroHLC ☭ <root@pedrohlc.com>
2023-03-26 21:27:09 -0300

    [flake] bump

--- shared/lib/4k-nohidpi.nix
+++ shared/lib/4k-nohidpi.nix
@@ -1,7 +1,6 @@
-# Decent 4K display with no HiDPI
+# Decent 4K display with no HiDPI scaling
 { pkgs, ... }:
 {
-  hardware.video.hidpi.enable = false;
   fonts.fontconfig = {
     antialias = true;
     subpixel = {
