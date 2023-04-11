3a2de10062a5fce382637a35489dff2c71a59263
PedroHLC ☭ <root@pedrohlc.com>
2023-04-11 15:23:17 -0300

    [core] enhance locales; [flake] bump

--- systems/core-configuration.nix
+++ systems/core-configuration.nix
@@ -60,10 +60,24 @@
 
   # Internationalisation.
   i18n = {
-    defaultLocale = "en_GB.UTF-8";
-    supportedLocales = [ "en_GB.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" "pt_BR.UTF-8/UTF-8" ];
+    supportedLocales = [ "en_IE.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" "pt_BR.UTF-8/UTF-8" ];
+    # For apps using LANG: the closest to what I have with LC_* below
+    defaultLocale = "en_IE.UTF8";
+    # For apps using LC_*:
     extraLocaleSettings = {
-      LC_TIME = "pt_BR.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+
+      LC_CTYPE = "pt_BR.UTF8";
+      LC_NUMERIC = "pt_BR.UTF8";
+      LC_TIME = "pt_BR.UTF8";
+      LC_COLLATE = "pt_BR.UTF8";
+      LC_MONETARY = "pt_BR.UTF8";
+      LC_PAPER = "pt_BR.UTF8";
+      LC_NAME = "pt_BR.UTF8";
+      LC_ADDRESS = "pt_BR.UTF8";
+      LC_TELEPHONE = "pt_BR.UTF8";
+      LC_MEASUREMENT = "pt_BR.UTF8";
+      LC_IDENTIFICATION = "pt_BR.UTF8";
     };
   };
   console.font = "Lat2-Terminus16";
