80b12f75cdcd1cdb59e64f8f1d579d8b2c9c02b3
PedroHLC ☭ <root@pedrohlc.com>
2023-05-27 17:15:33 -0300

    [core] looks like using extra-substituters is wrong now

--- systems/core-configuration.nix
+++ systems/core-configuration.nix
@@ -28,11 +28,11 @@
     settings.max-jobs = "auto";
 
     # github:nix-community/* cache
-    settings.extra-substituters = [
+    settings.substituters = [
       "https://nix-community.cachix.org"
       "https://nyx.chaotic.cx"
     ];
-    settings.extra-trusted-public-keys = [
+    settings.trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
