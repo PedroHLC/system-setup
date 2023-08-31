e50efbbd97f3b1852df36b1f4b6c0ba30d36894a
PedroHLC ☭ <root@pedrohlc.com>
2023-08-31 10:37:35 -0300

    [flake] bump

--- systems/core-configuration.nix
+++ systems/core-configuration.nix
@@ -39,7 +39,7 @@
 
     # github:nix-community/* cache
     settings.substituters = [
-      "https://nix-community.cachix.org"
+      "https://nix-community.cachix.org/"
     ];
     settings.trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
