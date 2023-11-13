e165f49ac8206546f89ec6d1577697432f79a462
PedroHLC ☭ <root@pedrohlc.com>
2023-11-13 10:06:06 -0300

    [scripts] make alternative-session reproducible

--- shared/scripts/alternative-session.sh
+++ shared/scripts/alternative-session.sh
@@ -1,70 +1,108 @@
 #!/usr/bin/env bash
 
+# Check dependencies + reproducible
+set -eu
+BASH=${BASH:-$(which bash)}
+FISH=${FISH:-$(which fish)}
+FLATPAK=${FLATPAK:-$(which flatpak)}
+FZF=${FZF:-$(which fzf)}
+GAMESCOPE=${GAMESCOPE:-$(which gamescope)}
+GAMESCOPE_NOWRAP=${GAMESCOPE_NOWRAP:-$(which gamescope)}
+GREP=${GREP:-$(which grep)}
+LSMOD=${LSMOD:-$(which lsmod)}
+STEAM=${STEAM:-$(which steam)}
+STEAM_GAMESCOPE=${STEAM_GAMESCOPE:-$(which steam-gamescope)}
+SYSTEMCTL=${SYSTEMCTL:-$(which systemctl)}
+TMUX=${TMUX:-$(which tmux)}
+WPCTL=${WPCTL:-$(which wpctl)}
+
+_PARENT_PATH="$PATH"
+PATH=${COREUTILS:-$(dirname $(which env))}
+
+function ext() {
+  env PATH="$_PARENT_PATH" "${_PRE[@]}" "$@"
+}
+set +e
+
+# Prepare FZF menu options
 OPTIONS="nixpkgs-steam-sdr
-flatpak-steam-sdr
 flatpak-heroic-sdr
 tmux
 fish
 bash
 poweroff"
+#flatpak-steam-sdr
 
 OPTIONS_HDR=""
-if lsmod | grep -wq "amdgpu"; then
+if "$LSMOD" | "$GREP" -wq "amdgpu"; then
   OPTIONS_HDR="nixpkgs-steam-hdr
-flatpak-steam-hdr
 flatpak-heroic-hdr
 "
+#flatpak-steam-hdr
 fi
 
+# Helper functions
 function flatpak-steam() {
-  _OVERRIDES=$(flatpak override --user --show com.valvesoftware.Steam)
+  _OVERRIDES=$("$FLATPAK" override --user --show com.valvesoftware.Steam)
   if [[ -z $_OVERRIDES ]]; then
-    flatpak override --user --filesystem=/mnt com.valvesoftware.Steam
+    "$FLATPAK" override --user --filesystem=/mnt com.valvesoftware.Steam
   fi
 
-  env -u LD_PRELOAD "${_PRE[@]}" flatpak run com.valvesoftware.Steam -tenfoot -pipewire-dmabuf "${@}"
+  ext -u LD_PRELOAD "$FLATPAK" run com.valvesoftware.Steam -tenfoot -pipewire-dmabuf "${@}"
 }
 
-function flatpak-heroic() {
-  env -u LD_PRELOAD "${_PRE[@]}" flatpak run com.heroicgameslauncher.hgl "${@}"
+function "$FLATPAK"-heroic() {
+  ext -u LD_PRELOAD "$FLATPAK" run com.heroicgameslauncher.hgl "${@}"
 }
 
+function raise-volume() {
+  "$WPCTL" set-volume '@DEFAULT_SINK@' '100%' || true
+}
+
+# The menu and its actions
 while true; do
   _PRE=()
-  case $(fzf <<<"${OPTIONS_HDR}${OPTIONS}") in
+  case $("$FZF" <<<"${OPTIONS_HDR}${OPTIONS}") in
     nixpkgs-steam-sdr)
-      gamescope --steam --disable-color-management -- steam -tenfoot -pipewire-dmabuf
+      raise-volume
+      ext "$GAMESCOPE_NOWRAP" --steam --disable-color-management -- steam -tenfoot -pipewire-dmabuf
       ;;
     nixpkgs-steam-hdr)
-      DXVK_HDR=1 steam-gamescope
+      raise-volume
+      ext DXVK_HDR=1 "${STEAM_GAMESCOPE}"
       ;;
     flatpak-steam-sdr)
-      _PRE=(gamescope --steam --disable-color-management --)
-      flatpak-steam
+      raise-volume
+      _PRE=("$GAMESCOPE_NOWRAP" --steam --disable-color-management --) \
+        flatpak-steam
       ;;
     flatpak-steam-hdr)
-      _PRE=(DXVK_HDR=1 gamescope --steam --hdr-enabled --)
-      flatpak-steam
+      raise-volume
+      _PRE=(DXVK_HDR=1 gamescope --steam --hdr-enabled --) \
+        flatpak-steam
       ;;
     flatpak-heroic-sdr)
-      _PRE=(gamescope --)
-      flatpak-heroic
+      raise-volume
+      _PRE=("$GAMESCOPE_NOWRAP" --) \
+        flatpak-heroic
       ;;
     flatpak-heroic-hdr)
-      _PRE=(DXVK_HDR=1 gamescope --hdr-enabled --)
-      flatpak-heroic
+      raise-volume
+      _PRE=(DXVK_HDR=1 gamescope --hdr-enabled --) \
+        flatpak-heroic
       ;;
     tmux)
-      tmux -l
+      ext "$TMUX" -l
       ;;
     fish)
-      fish -l
+      ext "$FISH" -l
       ;;
     bash)
-      bash -l
+      # can't "-login" here, it will trigger .profile
+      ext "$BASH"
       ;;
     poweroff)
-      exec systemctl poweroff
+      exec "$SYSTEMCTL" poweroff
       ;;
     *)
       break
