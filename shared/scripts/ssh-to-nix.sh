5a456a4e97ea6bf095cf96ac9de3fe7ef0fa4db8
PedroHLC ☭ <root@pedrohlc.com>
2023-12-20 16:18:59 -0300

    [flake] hm-infect package (4)

--- shared/scripts/ssh-to-nix.sh
+++ shared/scripts/ssh-to-nix.sh
@@ -3,4 +3,4 @@ exec ssh "$@" -- nix \
   --extra-experimental-features nix-command \
   --extra-experimental-features flakes \
   --accept-flake-config \
-  run --impure 'github:PedroHLC/system-setup#pedrohlc-hm-infect'
+  run --impure --refresh 'github:PedroHLC/system-setup#pedrohlc-hm-infect'
