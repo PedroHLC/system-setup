f8f27b62eb18c607f882f5a68825c41ac508f68c
PedroHLC <root@pedrohlc.com>
2024-05-18 20:53:28 -0300

    [vps] add CTR server (4)

--- systems/vps-lab/servers/ctr.nix
+++ systems/vps-lab/servers/ctr.nix
@@ -10,6 +10,8 @@ let
       ExecStart = "${flakes.ctr.packages.${pkgs.system}.online-server.release.native32.gcc}/bin/ctr_srv -p 6500${room}";
       Restart = "always";
       RestartSec = "8";
+      StandardOutput = "syslog";
+      StandardError = "syslog";
     };
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
@@ -18,9 +20,9 @@ let
 in
 {
   systemd.services = {
-    ctr1 = ctrService1;
-    ctr2 = ctrService2;
-    ctr3 = ctrService3;
-    ctr4 = ctrService4;
+    ctr1 = ctrService "1";
+    ctr2 = ctrService "2";
+    ctr3 = ctrService "3";
+    ctr4 = ctrService "4";
   };
 }
