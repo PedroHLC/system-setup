cf829e0a3bbf3c06e2ca80c8e8ebc7e7bd4c5db3
PedroHLC <root@pedrohlc.com>
2024-07-18 16:26:43 -0300

    [flake] bump; [code] add Lix cache; [vps] enable matrix presence

--- nixos/modules/configs/core.nix
+++ nixos/modules/configs/core.nix
@@ -37,9 +37,11 @@
     # github:nix-community/* cache
     settings.substituters = [
       "https://nix-community.cachix.org/"
+      "https://cache.lix.systems"
     ];
     settings.trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
     ];
   };
 
