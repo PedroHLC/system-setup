1ab6892cc3d26d654eb8083126dbd1012c707782
PedroHLC <root@pedrohlc.com>
2025-02-03 09:08:19 -0300

    [flake] restore right channel; bump

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   # My main channel and extra repositories
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.follows = "chaotic/nixpkgs";
 
     # Reset rootfs every reboot
     impermanence.url = "github:nix-community/impermanence";
