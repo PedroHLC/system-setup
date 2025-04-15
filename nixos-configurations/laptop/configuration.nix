f9669ba5883ebf2f0b5b74b49bebbccfd4e566de
PedroHLC <root@pedrohlc.com>
2025-04-14 21:58:44 -0300

    [laptop] avoid forcing VK to Intel

--- nixos-configurations/laptop/configuration.nix
+++ nixos-configurations/laptop/configuration.nix
@@ -39,7 +39,8 @@
     ];
     # Prefer intel unless told so
     variables = {
-      "VK_DRIVER_FILES" = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/intel_icd.i686.json";
+      # Issue: Sway isn't starting when I force Vulkan to Intel here
+      # "VK_DRIVER_FILES" = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/intel_icd.i686.json";
       "LIBVA_DRIVER_NAME" = "iHD";
     };
     # Helps me debugging kernel modules
