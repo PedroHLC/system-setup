2b9ae547d4dd7c4e8003fde2c2274b3eecf82e1c
PedroHLC <root@pedrohlc.com>
2025-06-01 21:09:00 -0300

    [flake] bump; [laptop] unpatch nvidia

--- nixos-configurations/laptop/configuration.nix
+++ nixos-configurations/laptop/configuration.nix
@@ -128,33 +128,11 @@
   hardware.nvidia =
     let
       # Preferred NVIDIA Version
-      nvidiaPackage =
-        if config.boot.kernelPackages.nvidiaPackages.latest.version == "570.153.02" then
-          nvidiaPatched
-        else
-          config.boot.kernelPackages.nvidiaPackages.latest;
-
-      nvidiaPatched = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "575.57.08";
-        sha256_64bit = "sha256-KqcB2sGAp7IKbleMzNkB3tjUTlfWBYDwj50o3R//xvI=";
-        sha256_aarch64 = "sha256-VJ5z5PdAL2YnXuZltuOirl179XKWt0O4JNcT8gUgO98=";
-        openSha256 = "sha256-DOJw73sjhQoy+5R0GHGnUddE6xaXb/z/Ihq3BKBf+lg=";
-        settingsSha256 = "sha256-AIeeDXFEo9VEKCgXnY3QvrW5iWZeIVg4LBCeRtMs5Io=";
-        persistencedSha256 = "sha256-Len7Va4HYp5r3wMpAhL4VsPu5S0JOshPFywbO7vYnGo=";
-
-        patches = [ gpl_symbols_linux_615_patch ];
-      };
-
-      gpl_symbols_linux_615_patch = pkgs.fetchpatch {
-        url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
-        hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
-        stripLen = 1;
-        extraPrefix = "kernel/";
-      };
+      nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.latest;
     in
     {
       package = nvidiaPackage;
-      open = false;
+      open = true;
     };
 
   # Creates a second boot entry with proprietary NVIDIA GPU (PRIME Offloading + Wayland)
