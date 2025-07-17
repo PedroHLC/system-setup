753c91cf75b4149e6214835e295e3131f18b8bf2
PedroHLC <root@pedrohlc.com>
2025-07-16 21:06:40 -0300

    [desktop] enable DXVK_HDR for TV

--- home-configurations/pedrohlc/default.nix
+++ home-configurations/pedrohlc/default.nix
@@ -88,9 +88,7 @@ with utils; {
 
           steamSession = with steamMachine; ''
             if ${check-sha256} '${sha256}' '/sys/class/drm/${output}/edid' ${salt}; then
-              steam-gamescope
-              # it crashed, wait before looping
-              sleep 12
+              exec env DXVK_HDR=1 steam-gamescope
             else
               ${deSession}
             fi
