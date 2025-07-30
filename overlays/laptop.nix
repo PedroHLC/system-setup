0e2a87775155a0d45722ca08b72c897ddd9c2fad
PedroHLC <root@pedrohlc.com>
2025-07-30 20:55:40 -0300

    [laptop] cut older GSP on newer kernel

--- overlays/laptop.nix
+++ overlays/laptop.nix
@@ -1,13 +1,23 @@
 final: prev: {
   # Bring GSP and related GPU stuff, but without any other GPU
-  makeModulesClosure = args: (prev.makeModulesClosure args).overrideAttrs (prevAttrs: {
-    builder = final.writeShellScript "modules-closure.sh" ((builtins.readFile prevAttrs.builder) + ''
-      mv "$out/lib/firmware/nvidia" "$out/lib/firmware/_nvidia"
-      mkdir "$out/lib/firmware/nvidia"
-      mv "$out/lib/firmware/_nvidia/ga10"{2,7} "$out/lib/firmware/nvidia/"
-      rm -rf "$out/lib/firmware/_nvidia"
-    '');
-  });
+  makeModulesClosure = args: (prev.makeModulesClosure args).overrideAttrs (prevAttrs:
+    let
+      inherit (final.lib.strings) removePrefix versionAtLeast optionalString;
+      kernelVersion = removePrefix "linux-" args.kernel.name;
+      versionToDelete = if versionAtLeast kernelVersion "6.16" then "535" else null;
+    in
+    {
+      builder = final.writeShellScript "modules-closure.sh" (
+        (builtins.readFile prevAttrs.builder) + optionalString (versionToDelete != null) ''
+          rm "$out/lib/firmware/nvidia/ga10"{2,7}/gsp/*-${versionToDelete}*.bin.zst
+        '' + ''
+          mv "$out/lib/firmware/nvidia" "$out/lib/firmware/_nvidia"
+          mkdir "$out/lib/firmware/nvidia"
+          mv "$out/lib/firmware/_nvidia/ga10"{2,7} "$out/lib/firmware/nvidia/"
+          rm -rf "$out/lib/firmware/_nvidia"
+        ''
+      );
+    });
 
   # Allow steam to find nvidia-offload script
   steam = prev.steam.override {
