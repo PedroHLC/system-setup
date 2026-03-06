a2e4ba2e2e14e0462eb6586c00a71c0dd2e012c8
PedroHLC <root@pedrohlc.com>
2026-03-06 12:00:14 -0300

    [vps] fix tuwunel

--- nixos-configurations/vps-lab/servers/matrix.nix
+++ nixos-configurations/vps-lab/servers/matrix.nix
@@ -34,6 +34,7 @@ in
       database_backend = "rocksdb";
       trusted_servers = [ "envs.net" ];
       sentry = true;
+      cache_capacity_modifier = 5.0;
     };
   };
   systemd.services.conduit.serviceConfig.ExecStart = lib.mkForce binary;
